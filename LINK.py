9fde77cfd616b9e4cdcbc78d31f5702ad6ae7534
Bluejay <collinbrahana@gmail.com>
2020-07-05 13:13:38 -0700

    Why won't the database connect now
    Gitignore, maybe?

--- LINK.py
+++ LINK.py
@@ -6,9 +6,16 @@ Created on Fri Jul  3 11:46:51 2020
 @author: collinbrahana
 """
 from NODEdata import LINKdata
+from NODE import DBapi
 
 
-class LINK:
+class LINK(DBapi):
     def __init__(self):
         self.ld = LINKdata("", "", 0)
-        pass
+        return None
+
+    def setLINK(self,NIN1,NIN2,LINKTYPE):
+        self.ld.NIN_1 = NIN1
+        self.ld.NIN_2 = NIN2
+        self.ld.LINKTYPE = LINKTYPE
+        
--- NODE.py
+++ NODE.py
@@ -20,13 +20,13 @@ class DBapi:
 
     def connectDATABASE(self):
         self.connection = sqlite3.connect("Minddancer.db")
-        self.cursor = self.connection.cursor()
+        self.cursor = self.connection.cursor
         return None
 
     def checkNODEexistance(self, tgt_NIN):
         t = (tgt_NIN,)
         self.cursor.execute("SELECT * FROM NODES WHERE NIN = ?", t)
-        if self.cursor.fetchone() == None:
+        if self.cursor.fetchone == None:
             return 0
         else:
             return 1
@@ -34,62 +34,80 @@ class DBapi:
     def exportNODEtoDATABASE(self):  #Only call this to make a new node, NOT TO UPDATE
         t = (self.dc.NIN, self.dc.NODETYPE, self.dc.NAME, self.dc.DATA,)
         self.cursor.execute("INSERT INTO NODES VALUES (?,?,?,?)", t)
-        self.connection.commit()
+        self.connection.commit
 #        if self.checkNODEexistance(self.dc.NIN) == 1:
 #            raise ProjectErrors.DATABASEexportERROR
         return None
 
     def fetchNODEfromDATABASE(self, tgt_NIN):
-        self.connectDATABASE()
+        self.connectDATABASE
         t = (tgt_NIN,)
         output = self.cursor.execute("SELECT * FROM NODES WHERE NIN=?", t) #N0000011
-        outtuple = output.fetchone()
+        outtuple = output.fetchone
         self.dc.NIN = outtuple[0]
         self.dc.NODETYPE = outtuple[1]
         self.dc.NAME = outtuple[2]
         self.dc.DATA = outtuple[3]
-        self.closeDATABASE()
+        self.closeDATABASE
         return outtuple
 
-    def updateDATABASE(self, NT, NA, D, NIN):
-        self.connectDATABASE()
+    def fetchLINKLISTfromDATABASE(self, tgt_NIN):
+        self.connectDATABASE
+        t = (tgt_NIN, )
+        cmd = "SELECT * FROM NODES WHERE NIN_ONE=?"
+        self.cursor.execute(cmd, t)
+        print(self.cursor.fetchall())
+        
+    def updateNODES(self, NT, NA, D, NIN):
+        self.connectDATABASE
         t = (NT, NA, D, NIN, )
         cmd = "UPDATE NODES SET NODETYPE=?, NAME=?, DATA=? WHERE NIN=?"
         self.cursor.execute(cmd, t)
-        self.connection.commit()
-        self.closeDATABASE()
+        self.connection.commit
+        self.closeDATABASE
+    
+    def updateLINKS(self,N1,N2,LT):
+        self.connectDATABASE
+        LT2 = -1*LT
+        t = (N1,N2,LT)
+        t2 = (N2,N1,LT2)
+        cmd = "UPDATE LINKS SET LINK_ONE=?, LINK_TWO=?, LINKTYPE=?"
+        self.cursor.execute(cmd, t)
+        self.cursor.execute(cmd, t2)
+        self.connection.commit
+        self.closeDATABASE
 
     def closeDATABASE(self):
-        self.connection.close()
+        self.connection.close
         return None
 
 
 class Node(DBapi):
     def __init__(self, createtype):  #N0000012
         self.dc = NODEdata("", 0, "", "", []) #Create instance of Node Dataclass for storage internally
-        iDB.createTABLES() # I should move this to startup, we don't need to run it for every node, just an existance check
-        self.connectDATABASE()
+        iDB.createTABLES # I should move this to startup, we don't need to run it for every node, just an existance check
+        self.connectDATABASE
         if createtype != 0:  #Looking for a string NIN here
             if self.checkNODEexistance(createtype) == 1: #Makes sure node in DB exists                
                 temp_node_tuple = self.fetchNODEfromDATABASE(createtype)
                 self.dc.NIN = temp_node_tuple[0]
                 self.changeNODETYPE(temp_node_tuple[1])
                 self.changeNAME(temp_node_tuple[2])
-                self.closeDATABASE()
+                self.closeDATABASE
             else:
-                self.closeDATABASE()
+                self.closeDATABASE
                 raise ProjectErrors.DATABASEretrivalERROR
         else:
-            self.makeNIN()
+            self.makeNIN
             self.dc.NIN = str(self.dc.NIN)
-            self.exportNODEtoDATABASE()
-            self.closeDATABASE()
+            self.exportNODEtoDATABASE
+            self.closeDATABASE
         return None
 
     # Memory Structures
     def makeNIN(self):  # N0000006
         if self.dc.NIN == "":
-            self.dc.NIN = uuid.uuid4()
+            self.dc.NIN = uuid.uuid4
         else:
             raise ProjectErrors.IDENTIFIERassignmentERROR
 
@@ -99,18 +117,17 @@ class Node(DBapi):
 
     def changeNAME(self, new_name):
         self.dc.NAME = new_name
-        self.updateDATABASE(self.dc.NODETYPE, self.dc.NAME, self.dc.DATA, self.dc.NIN)
+        self.updateNODES(self.dc.NODETYPE, self.dc.NAME, self.dc.DATA, self.dc.NIN)
         return None
 
     def changeNODETYPE(self, new_type): 
         self.dc.NODETYPE = new_type
-        self.updateDATABASE(self.dc.NODETYPE, self.dc.NAME, self.dc.DATA, self.dc.NIN)
+        self.updateNODES(self.dc.NODETYPE, self.dc.NAME, self.dc.DATA, self.dc.NIN)
         return None
 
     def returnNIN(self):
         return self.dc.NIN
 
-    # Database Structures       
     def DeleteLINK(self, tgt_NIN_1, tgt_NIN_2, tgt_NODETYPE):
         # look in node for (NIN_1,NIN_2,NODETYPE) and (NIN_2,NIN_1, -1*NODETYPE)
         # If it exists, perform deletion operation. If neither exist, raise an error.
@@ -118,3 +135,5 @@ class Node(DBapi):
 
     def fetchNODEfromMEMORY(self):
         pass
+
+node = Node(0)
